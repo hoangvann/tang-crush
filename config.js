96e014852008aa9c42dac448aa867368941ea965
hoangvann <86727608+hoangvann@users.noreply.github.com>
2021-06-30 20:56:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'Hellu cậu!',
+    introDesc: `Tớ có chuyện này muốn
+    nói với cậu lâu lắm rồi!!
+    Cậu phải trả lời thật lòng đấy nha`,
+    btnIntro: 'OK',
+    title: 'Tớ thích cậu lâu lắm rồi 😙',
+    desc: 'Nếu cậu không trả lời hoặc thoát web cậu là ny tớ',
+    btnYes: 'Tớ cũng thích cậu lâu rồi <33',
+    btnNo: 'Điên à mơ đi :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
